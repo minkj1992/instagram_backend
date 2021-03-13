e07457dce9c55812e25342d9053eff9bcf539de2
minkj1992 <minkj1992@gmail.com>
2021-03-13 23:22:06 +0900

    feat: createMovie mutation graphql

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 import { ApolloServer, gql } from "apollo-server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const typeDefs = gql`
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    updateMovie(title: String!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hi world",
+    movies: () => prisma.movie.findMany(),
+    movie: () => ({ title: "dummy movie", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: async (_, { title, year, genre }) => {
+      return await prisma.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      });
+    },
+
+    updateMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
