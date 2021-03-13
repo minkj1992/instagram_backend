89b26fd472ac0ad98d2e91f3066b6e5bd1507673
minkj1992 <minkj1992@gmail.com>
2021-03-14 02:03:29 +0900

    fix: loadFiles 할 때, space 제거

--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,11 @@
+import path from "path";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedTypes = loadFilesSync(path.join(__dirname, "./**/*.typeDefs.js"));
 const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries, mutations}.js`
+  path.join(__dirname, "./**/*.{queries,mutations}.js")
 );
 
 const typeDefs = mergeTypeDefs(loadedTypes);
