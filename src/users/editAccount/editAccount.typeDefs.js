77face65c29de3aa8db5d7f914266c3f0219225b
minkj1992 <minkj1992@gmail.com>
2021-03-14 19:27:10 +0900

    feat: edit account from user entity

---
+++ src/users/editAccount/editAccount.typeDefs.js
@@ -0,0 +1,18 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type EditAccountResult {
+    ok: Boolean!
+    error: String
+  }
+
+  type Mutation {
+    editAccount(
+      firstName: String
+      lastName: String
+      username: String
+      email: String
+      password: String
+    ): EditAccountResult
+  }
+`;
--- src/users/users.typeDefs.js
+++ src/users/users.typeDefs.js
@@ -13,7 +13,6 @@ export default gql`
       email: String!
       password: String!
     ): User
-
     login(username: String!, password: String!): LoginResult
   }
 
